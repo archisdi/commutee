98608c5099810ebb5292ddce509d8e112c927811
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2024-09-22 13:50:41 +0700

    feat: add loading and refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
+  // IonIcon,
+  // IonLabel,
+  // IonRouterOutlet,
+  // IonTabBar,
+  // IonTabButton,
+  // IonTabs,
   setupIonicReact
 } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+// import { IonReactRouter } from '@ionic/react-router';
+// import { ellipse, square, triangle } from 'ionicons/icons';
+import Main from './pages/Main';
+// import Tab2 from './pages/Tab2';
+// import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -49,10 +49,7 @@ setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-
-    {/* use Tab 1*/}
-    <Tab1 />
-
+    <Main />
     {/* <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
