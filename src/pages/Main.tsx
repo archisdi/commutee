d26d798d76e37f6ce0fca803fcf67500d0538bef
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2024-09-22 22:02:45 +0700

    feat: record historical search

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from 'react';
 import {
   IonContent, IonPage, IonCard, IonCardContent, IonText, IonRow, IonCol, IonGrid,
   IonRefresher, IonRefresherContent, useIonLoading, IonModal, IonHeader, IonToolbar,
-  IonTitle, IonButtons, IonButton, IonIcon, IonList, IonItem, IonSearchbar
+  IonTitle, IonButtons, IonButton, IonIcon, IonList, IonItem, IonSearchbar, IonItemDivider,
+  IonItemGroup, IonLabel
 } from '@ionic/react';
 
 import { caretDownSharp } from 'ionicons/icons';
@@ -27,7 +28,6 @@ interface Station {
   sta_name: string;
   group_wil: number;
   fg_enable: number;
-  is_favorite?: boolean;
 }
 
 const JWT_TOKEN = import.meta.env.VITE_JWT_TOKEN;
@@ -40,8 +40,10 @@ const Main: React.FC = () => {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   const [station, setStation] = useState<Station[]>([]);
-  const [selectedStation, setSelectedStation] = useState<Station | null>(null);
   const [trainSchedule, setTrainSchedule] = useState<TrainSchedule[]>([]);
+
+  const [selectedStation, setSelectedStation] = useState<Station | null>(null);
+  const [historyStation, setHistoryStation] = useState<Station[]>([]);
 
   const getStation = async () => {
     const cacheStations = localStorage.getItem('stations');
@@ -104,6 +106,13 @@ const Main: React.FC = () => {
     }
   }
 
+  const getHistoryStation = () => {
+    const history = localStorage.getItem('historyStation');
+    if (history) {
+      setHistoryStation(JSON.parse(history));
+    }
+  }
+
   const handleRefresh = (e: CustomEvent) => {
     getStationSchedule(selectedStation!);
     e.detail.complete();
@@ -127,17 +136,34 @@ const Main: React.FC = () => {
     setStation(filteredStation);
   }
 
-  const handleSelectStation = (station: Station) => {
+  const handleSelectStation = (station: Station, isHistory?: boolean) => {
     selectStation(station);
     setIsSearchOpen(false);
 
     /** reset filtered station */
     getStation();
+
+    /** add to history */
+    if (!isHistory) {
+      const history = historyStation;
+      const isExist = historyStation.find((sta) => sta.sta_id === station.sta_id);
+      if (isExist) return;
+
+      history.push(station);
+
+      if (history.length > 5) {
+        history.shift();
+      }
+
+      localStorage.setItem('historyStation', JSON.stringify(history));
+      setHistoryStation(history);
+    }
   }
 
   useEffect(() => {
     loadLastSelectedStation();
     getStation();
+    getHistoryStation();
   }, []);
 
   useEffect(() => {
@@ -151,7 +177,51 @@ const Main: React.FC = () => {
     return `${time} min`;
   };
 
-  const searchModal =
+  const renderStationHistoryList = () => {
+    if (!historyStation.length) return null;
+    return (
+      <IonList>
+        <IonItemGroup>
+          <IonItemDivider>
+            <IonLabel>Terakhir Dipilih</IonLabel>
+          </IonItemDivider>
+          {historyStation.map((station) => (
+            <IonItem key={station.sta_id} onClick={() => handleSelectStation(station)}>
+              <IonRow>
+                {station.sta_name}
+              </IonRow>
+            </IonItem>
+          )).reverse()}
+        </IonItemGroup>
+      </IonList>
+    )
+  }
+
+  const renderStationList = () => {
+    const historyMap = historyStation.reduce((acc: any, station) => {
+      acc[station.sta_id] = station;
+      return acc;
+    }, {});
+
+    const divider = historyStation.length
+      ? <IonItemDivider><IonLabel>Semua Stasiun</IonLabel></IonItemDivider>
+      : null;
+
+    return (
+      <IonList>
+        <IonItemGroup>
+          {divider}
+          {station.filter(s => !historyMap[s.sta_id]).map((station) => (
+            <IonItem key={station.sta_id} onClick={() => handleSelectStation(station)}>
+              {station.sta_name}
+            </IonItem>
+          ))}
+        </IonItemGroup>
+      </IonList>
+    )
+  }
+
+  const selectStationModal =
     <IonModal isOpen={isSearchOpen}>
       <IonHeader>
         <IonToolbar>
@@ -163,13 +233,8 @@ const Main: React.FC = () => {
       </IonHeader>
       <IonContent className="ion-padding">
         <IonSearchbar debounce={500} onIonInput={(ev) => handleSearchStation(ev)} />
-        <IonList>
-          {station.map((station) => (
-            <IonItem key={station.sta_id} onClick={() => handleSelectStation(station)}>
-              {station.sta_name}
-            </IonItem>
-          ))}
-        </IonList>
+        {renderStationHistoryList()}
+        {renderStationList()}
       </IonContent>
     </IonModal>
 
@@ -217,7 +282,7 @@ const Main: React.FC = () => {
           <IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
             <IonRefresherContent />
           </IonRefresher>
-          {searchModal}
+          {selectStationModal}
           {stations}
           {schedules}
         </Frame>
